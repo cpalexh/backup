ea7c89e6ce9a396e15d39f9c15633fcc94bd71d9
cpalexh <alex.huxel@coding-pioneers.com>
2024-08-24 01:12:55 +0200

    add escaping for security reasons, and modify the file on some points

--- backup.php
+++ backup.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Backup Script
  * Description: Automatic DB backup generator
- * Version: 1.1.2
+ * Version: 1.2
  * Author: Alexander Huxel
  * Author URI: https://webentwicklung-huxel.de
  * License: MIT License
@@ -25,12 +25,11 @@ class BackupScript {
     private $mysql_file;
     private $log_file;
 
-
     public function __construct() {
         $this->db_name = env('DB_NAME');
         $this->db_passwd = env('DB_PASSWORD');
-        $this->backup_folder = $_SERVER['DOCUMENT_ROOT'] . '/../backups/' . '/' . date('Y-m-d') . '/';
-        $this->mail = env('BACKUP_MAIL') ? env('BACKUP_MAIL') : "admin@webentwicklung-huxel.de";
+        $this->backup_folder = $this->getBackupFolder();
+        $this->mail = filter_var(env('BACKUP_MAIL'), FILTER_VALIDATE_EMAIL) ? env('BACKUP_MAIL') : "admin@webentwicklung-huxel.de";
         $this->message = "";
         $this->mysql_file = null;
         $this->log_file = WP_CONTENT_DIR . '/backup-script-log.txt';
@@ -38,105 +37,114 @@ class BackupScript {
         $this->generateBackup();
     }
 
-    private function createBackupFolder(){
-        if (!file_exists($this->backup_folder)) {
-            mkdir($this->backup_folder, 0755, true);
+    private function getBackupFolder() {
+        $folder = $_SERVER['DOCUMENT_ROOT'] . '/../backups/' . date('Y-m-d') . '/';
+        if (!file_exists($folder)) {
+            mkdir($folder, 0755, true);
+        }
+        return $folder;
+    }
+
+    private function sendMail() {
+        if (!$this->mail) {
+            $this->writeToLog("ERROR: Invalid email address provided for backup notification.");
+            return;
+        }
+
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host = env('MAIL_HOST'); 
+            $mail->SMTPAuth = true;
+            $mail->Username = env('MAIL_USERNAME');
+            $mail->Password = env('MAIL_PASSWORD');
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = 465;
+
+            $mail->setFrom(env('BACKUP_MAIL'), 'Backup Script');
+            $mail->addAddress($this->mail);
+
+            $mail->isHTML(true); 
+            $mail->Subject = 'DB Backup [' . date('d.m.Y H:i') . ']';
+            $mail->Body = $this->message;
+
+            $mail->send();
+            $this->writeToLog("Mail sent successfully.");
+        } catch (Exception $e) {
+            $this->writeToLog("ERROR: Email could not be sent. Mailer Error: {$mail->ErrorInfo}");
         }
     }
 
-    private function sendMail()
-    {
-        if (filter_var($this->mail, FILTER_VALIDATE_EMAIL)) {
-            $mail = new PHPMailer(true);
-    
-            try {
-                // Server settings
-                $mail->isSMTP();
-                $mail->Host       = env('MAIL_HOST'); 
-                $mail->SMTPAuth   = true;
-                $mail->Username   = env('MAIL_USERNAME');
-                $mail->Password   = env('MAIL_PASSWORD');
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-                $mail->Port       = 465;
-
-                $mail->setFrom(env('BACKUP_MAIL'), 'Backup Script');
-                $mail->addAddress($this->mail);
-    
-                // Content
-                $mail->isHTML(true); 
-                $mail->Subject = 'DB Backup [' . date('d.m.Y H:i') .']';
-                $mail->Body    = $this->message;
-    
-                $mail->send();
-            } catch (Exception $e) {
-                $this->writeToLog("ERROR: Email could not be sent. Mailer Error: {$mail->ErrorInfo}");
-            }
-        } else {
-            $this->writeToLog("ERROR: Email could not be sent due to invalid address!");
-        }
-    }
-
-    private function getCalculatedMysqlSize($inputfile){
+    private function getCalculatedMysqlSize($inputfile) {
         $file = $inputfile . ".gz";
-        $size = filesize($file);
+        $size = @filesize($file);
+        if ($size === false) {
+            $this->writeToLog("ERROR: Could not calculate file size for $file.");
+            return "Unknown size";
+        }
+
         $units = ["Bytes", "KB", "MB", "GB", "TB"];
         $size = round($size / pow(1024, ($i = floor(log($size, 1024)))), 2);
         $mysql_size = "$size {$units[$i]}";
-
         $this->writeToLog("Dump size: $mysql_size");
+
         return $mysql_size;
     }
 
-    private function generateMysqlFile(){
+    private function generateMysqlFile() {
         $this->mysql_file = $this->backup_folder . "{$this->db_name}_" . date('m_d_Y') . ".sql";
 
-        $this->createBackupFolder();        
-
-        if(!file_exists(dirname($this->mysql_file))) {
-            mkdir(dirname($this->mysql_file), 0777, true);
-        }
-
-        if(!file_exists($this->mysql_file) && !file_exists($this->mysql_file.'.gz')) {
-            exec("mysqldump -u {$this->db_name} -p'{$this->db_passwd}' --single-transaction --allow-keywords --complete-insert --insert-ignore --routines --events --force {$this->db_name} > $this->mysql_file");
-        }else{
+        if (file_exists($this->mysql_file . '.gz')) {
+            $this->writeToLog("Backup file already exists. Skipping creation.");
             return false;
         }
 
-        if(!file_exists($this->mysql_file.'.gz')) {
-            exec("gzip $this->mysql_file");
-        }else{
+        // Use PDO or MySQLi to create a database dump securely here.
+        $command = sprintf(
+            "mysqldump --user=%s --password=%s --single-transaction --allow-keywords --complete-insert --insert-ignore --routines --events --force %s > %s",
+            escapeshellarg($this->db_name),
+            escapeshellarg($this->db_passwd),
+            escapeshellarg($this->db_name),
+            escapeshellarg($this->mysql_file)
+        );
+
+        exec($command, $output, $return_var);
+
+        if ($return_var !== 0) {
+            $this->writeToLog("ERROR: Failed to create MySQL dump. Command: $command");
             return false;
         }
 
+        exec("gzip " . escapeshellarg($this->mysql_file));
         return true;
     }
 
-    private function backupMediaFiles(){
+    private function backupMediaFiles() {
         $upload_dir = wp_upload_dir()['basedir'];
         $media_backup_file = $this->backup_folder . "media_" . date('m_d_Y') . ".tar.gz";
 
-        exec("tar -czf $media_backup_file -C $upload_dir .");
+        exec("tar -czf " . escapeshellarg($media_backup_file) . " -C " . escapeshellarg($upload_dir) . " .");
+        $this->writeToLog("Media files backed up to: $media_backup_file");
 
-        // Log the location of the media backup
         return $media_backup_file;
     }
 
     private function generateBackup() {
-
-        if(!$this->generateMysqlFile()){
+        if (!$this->generateMysqlFile()) {
+            $this->writeToLog("Backup generation failed.");
             return false;
         }
 
-        $this->writeToLog("Backup completed successfully.");
-
-        $this->message =  '<h1>' . get_bloginfo("name") . '-Backup</h1>' . "<br>";
-        $this->message .= "Backup of database <b>{$this->db_name}</b> completed.<br>";
-        $this->message .= "Dump size: <b>" . $this->getCalculatedMysqlSize($this->mysql_file). "</b>.<br>";
-        $this->message .= date('d.m.Y H:i:s') . "<br>";
-        $this->message .= "Media files backed up to: " . $this->backupMediaFiles();
+        $this->message = sprintf(
+            '<h1>%s Backup</h1><br>Backup of database <b>%s</b> completed.<br>Dump size: <b>%s</b>.<br>%s<br>Media files backed up to: %s',
+            get_bloginfo("name"),
+            $this->db_name,
+            $this->getCalculatedMysqlSize($this->mysql_file),
+            date('d.m.Y H:i:s'),
+            $this->backupMediaFiles()
+        );
 
         $this->sendMail();
-        $this->writeToLog("Mail sent successfully.");
     }
 
     private function writeToLog($message) {
@@ -152,7 +160,6 @@ add_action('wp', function() {
         wp_schedule_event(time(), 'daily', 'backup_script_daily');
     }
 });
-
 
 add_action('backup_script_daily', function() {
     new BackupScript();
